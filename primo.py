b94369efa16f789975b236a8ec1a8635275ac187
franco <afrrcainecroi@gmail.com>
2024-02-13 12:52:06 +0100

    ddd

--- primo.py
+++ primo.py
@@ -1,1 +1,11 @@
+from math import sqrt
+
 print("Hello world")
+print(3**1001)
+print(sqrt(10))
+
+
+# Esercizio per casa
+# dato un triangolo rettangolo i cui cateti sono lunghi rispettivamente
+# 10.123 e 30.456
+# calcolare la lunghezza dell'ipotenusa
