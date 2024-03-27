b7aab4a27ccf817bf8a98ad30e94240c4c354d56
franco <afrrcainecroi@gmail.com>
2024-03-27 15:20:43 +0100

    27 marzo 2024

---
+++ 27032024-01.py
@@ -0,0 +1,8 @@
+# Ricordando che il MCD tra due numeri è il più grande numero che divide
+# i due numeri dati, scrivere un programma che calcola il MCD tra due numeri
+# letti da input
+# Rcordate!!! dati A e B, se esiste un numero M che li divide, allora lo stesso
+# numero dividerà A-B e B oppure B-A e A a seconda che A sia maggiore di
+# B o che B sia maggiore di A
+# Questa definizione ci consente di definire una soluzione che converge,
+# garantendo la terminazione dell'algoritmo
